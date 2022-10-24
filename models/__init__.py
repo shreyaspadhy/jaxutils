b73f46c97636819168d87b86d88105407fc23e28
shreyaspadhy <shreyaspadhy@gmail.com>
2022-10-24 09:05:59 +0000

    fixing imagenet folder location name

--- models/__init__.py
+++ models/__init__.py
@@ -5,4 +5,4 @@ __all__ = [
 
 from jaxutils.models.convert_utils import convert_lenet_param_keys, convert_resnet_param_keys, convert_model
 from jaxutils.models.lenets import conv3_block, conv5_block, LeNet, LeNetSmall, LeNetBig
-from jaxutils.models.resnets import ResNetBlock, BottleneckResNetBlock, ResNet, ResNet18
+from jaxutils.models.resnets import ResNetBlock, BottleneckResNetBlock, ResNet, ResNet18, ResNet50
